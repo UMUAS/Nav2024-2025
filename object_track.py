191ffd5f5187053f69d5173a59add920f29371da
Uriel Miller <umiller101@gmail.com>
2025-03-26 17:40:19 -0500

    Improved code format

--- object_track.py
+++ object_track.py
@@ -1,63 +1,83 @@
 import cv2 as cv
 import time
 
-tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
-tracker_type = tracker_types[4]
+TRACKER_TYPES = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
+RESOLUTION_SCALE = 0.75
+WINDOW_NAME = 'preview'
+TRACKER_TYPE = TRACKER_TYPES[4]
 
-tracker = None
-if tracker_type == 'BOOSTING':
-    tracker = cv.legacy.TrackerBoosting.create()
-if tracker_type == 'MIL':
-    tracker = cv.TrackerMIL.create()
-if tracker_type == 'KCF':
-    tracker = cv.TrackerKCF.create() 
-if tracker_type == 'TLD':
-    tracker = cv.legacy.TrackerTLD.create() 
-if tracker_type == 'MEDIANFLOW':
-    tracker = cv.legacy.TrackerMedianFlow.create() 
-if tracker_type == 'GOTURN':
-    tracker = cv.TrackerGOTURN.create()
-if tracker_type == 'MOSSE':
-    tracker = cv.legacy.TrackerMOSSE.create()
-if tracker_type == "CSRT":
-    tracker = cv.TrackerCSRT.create()
+bounding_box = None #This is the bounding box.
 
-initBB = None
-# fps = None
-prev_frame_time = 0 
-new_frame_time = 0
+def main():
+    """
+    Tracks a single object from live feed and updates bounding_box.
+    To determine what to track, press 's' and then hold and drag to define a region and press 'enter'.
+    To stop tracking (or to track again) press 's' again.
+    """
+    global bounding_box
 
-cv.namedWindow('preview')
-vc = cv.VideoCapture(0)
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
+    tracker = create_tracker(TRACKER_TYPE)
 
-while rval:
-    cv.imshow('preview', frame)
-    rval, frame = vc.read()
-    rescale = 0.75
-    frame = cv.resize(frame, (int(frame.shape[1]*rescale), int(frame.shape[0]*rescale)))
+    cv.namedWindow(WINDOW_NAME)
 
-    if(initBB is not None):
-        (success, box) = tracker.update(frame)
-        if(success):
-            (x, y, w, h) = [int(v) for v in box]
-            cv.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
-    new_frame_time = time.time()
-    fps = 1/(new_frame_time-prev_frame_time)
-    prev_frame_time = new_frame_time
+    video_capture = cv.VideoCapture(0)
+    if video_capture.isOpened(): # try to get the first frame
+        rval, frame = video_capture.read()
+    else:
+        rval = False
+    
+    prev_frame_time = 0
+    new_frame_time = 0
 
-    cv.putText(frame, str(int(fps)), (7, 70), cv.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 3, cv.LINE_AA)
+    while rval:
+        cv.imshow(WINDOW_NAME, frame)
+        rval, frame = video_capture.read()
+        frame = cv.resize(frame, (int(frame.shape[1]*RESOLUTION_SCALE), int(frame.shape[0]*RESOLUTION_SCALE)))
 
-    key = cv.waitKey(5)
-    if key == 27: # exit on ESC
-        break
-    elif key == ord('s'):
-        initBB = cv.selectROI('preview', frame, True, False)
-        if(initBB is not None):
-            tracker.init(frame, initBB)
+        if(bounding_box is not None):
+            (success, box) = tracker.update(frame)
+            if(success):
+                (x, y, w, h) = [int(v) for v in box]
+                cv.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+        new_frame_time = time.time()
+        fps = 1/(new_frame_time-prev_frame_time)
+        prev_frame_time = new_frame_time
 
-vc.release()
-cv.destroyWindow('preview')
+        cv.putText(frame, str(int(fps)), (7, 70), cv.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 100), 3, cv.LINE_AA)
+
+        key = cv.waitKey(5)
+        if key == 27: # exit on ESC
+            break
+        elif key == ord('s'):
+            if(bounding_box is not None):
+                bounding_box = None #Toggle off tracking.
+            else:
+                bounding_box = cv.selectROI(WINDOW_NAME, frame, showCrosshair=False, fromCenter=False)
+                if(bounding_box is not None):
+                    tracker.init(frame, bounding_box)
+
+    video_capture.release()
+    cv.destroyWindow(WINDOW_NAME)
+
+def create_tracker(tracker_type):
+    tracker = None
+    if tracker_type == 'BOOSTING':
+        tracker = cv.legacy.TrackerBoosting.create()
+    if tracker_type == 'MIL':
+        tracker = cv.TrackerMIL.create()
+    if tracker_type == 'KCF':
+        tracker = cv.TrackerKCF.create() 
+    if tracker_type == 'TLD':
+        tracker = cv.legacy.TrackerTLD.create() 
+    if tracker_type == 'MEDIANFLOW':
+        tracker = cv.legacy.TrackerMedianFlow.create() 
+    if tracker_type == 'GOTURN':
+        tracker = cv.TrackerGOTURN.create()
+    if tracker_type == 'MOSSE':
+        tracker = cv.legacy.TrackerMOSSE.create()
+    if tracker_type == "CSRT":
+        tracker = cv.TrackerCSRT.create()
+    return tracker
+
+if __name__ == "__main__":
+    main()

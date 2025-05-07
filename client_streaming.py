02a8c57860a87a37a02e82b1454e6ed912d03e7d
Uriel Miller <umiller101@gmail.com>
2025-05-07 12:38:16 -0500

    Added second camera

--- client_streaming.py
+++ client_streaming.py
@@ -1,21 +1,44 @@
 import cv2
 import socket
 import numpy as np
+import threading
 
 # UDP Configuration
 LISTEN_IP = '0.0.0.0'
-LISTEN_PORT = 5005
+LISTEN_PORT_1 = 5005
+LISTEN_PORT_2 = 5006
 
 MAX_PACKET_SIZE = 65507
-TIMEOUT_SECONDS = 10
+TIMEOUT_SECONDS = 3
 
 def main():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((LISTEN_IP, LISTEN_PORT))
-    print(f"Listening to {LISTEN_IP}:{LISTEN_PORT}")
+    sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
-    sock.settimeout(TIMEOUT_SECONDS)
+    sock1.bind((LISTEN_IP, LISTEN_PORT_1))
+    sock2.bind((LISTEN_IP, LISTEN_PORT_2))
+    
+    print(f"Listening to {LISTEN_IP}:{LISTEN_PORT_1}")
+    print(f"Listening to {LISTEN_IP}:{LISTEN_PORT_2}")
+    
+    sock1.settimeout(TIMEOUT_SECONDS)
+    sock2.settimeout(TIMEOUT_SECONDS)
 
+    t1 = threading.Thread(target=view_stream, args=(sock1, f'UDP Port {LISTEN_PORT_1}'))
+    t2 = threading.Thread(target=view_stream, args=(sock2, f'UDP Port {LISTEN_PORT_2}'))
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    sock1.close()
+    sock2.close()
+    cv2.destroyAllWindows()
+    print('Program ended.')
+
+def view_stream(sock, window_name):
     while True:
         try:
             data, _ = sock.recvfrom(65536)
@@ -27,7 +50,7 @@ def main():
             frame = cv2.imdecode(np_data, cv2.IMREAD_COLOR)
 
             if frame is not None:
-                cv2.imshow("UDP Video Stream", frame)
+                cv2.imshow(window_name, frame)
             else:
                 print("No Image!")
 
@@ -36,10 +59,6 @@ def main():
         except socket.timeout:
             print('Connection timeout! Ending program...')
             break
-
-    sock.close()
-    cv2.destroyAllWindows()
-    print('Program ended.')
 
 if(__name__ == '__main__'):
     main()
